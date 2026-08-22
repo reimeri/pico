4e103349572ce2fe8830e03f3891b639b7cb8871
reimeri <reimeri@tuta.io>
2026-08-22 17:41:12 +0300

    Reduce gap between footer and composer

--- app/app.c
+++ app/app.c
@@ -1131,7 +1131,7 @@ static Clay_RenderCommandArray CreateShellLayout(PicoApp *app)
          {.layout = {.layoutDirection = CLAY_TOP_TO_BOTTOM,
                      .sizing = {.width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0)},
                      .padding = {CONTENT_PADDING, 12, 16, 12},
-                     .childGap = 12},
+                     .childGap = 6},
           .backgroundColor = COLOR_BG})
     {
         CLAY(CLAY_ID("Body"),

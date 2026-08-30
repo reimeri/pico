addd5fef2be0e4b52f3717876f02d0c63c64da9a
reimeri <reimeri@tuta.io>
2026-08-30 15:55:39 +0300

    Fix v0.1.1 release

--- app/http.c
+++ app/http.c
@@ -5,7 +5,7 @@
 #include "json.h"
 
 #ifndef PICO_VERSION
-#define PICO_VERSION "0.1.0"
+#define PICO_VERSION "0.1.1"
 #endif
 
 #include <curl/curl.h>
--- app/plugin.c
+++ app/plugin.c
@@ -24,7 +24,7 @@
 #define PICO_CC "cc"
 #endif
 #ifndef PICO_VERSION
-#define PICO_VERSION "0.1.0"
+#define PICO_VERSION "0.1.1"
 #endif
 
 #define PICO_MAX_USER_PLUGINS 32
